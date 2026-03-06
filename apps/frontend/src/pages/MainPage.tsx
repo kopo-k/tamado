5ce5bd10b7e173a09b6b9a039c8ef5e2508fc46b
こぽー <tora1025oishi@gmail.com>
2026-03-06 10:48:43 +0900

    開発に必要なファイルを追加

---
+++ apps/frontend/src/pages/MainPage.tsx
@@ -0,0 +1,9 @@
+import { Header } from "@/components/layout/Header";
+
+export function MainPage() {
+  return (
+    <div className="min-h-screen bg-apple-bg dark:bg-apple-dark-bg">
+      <Header/>
+    </div>
+  )
+}
