b134d0a4d2eb13dce2ff95c401f37e7cf8c331d2
こぽー <tora1025oishi@gmail.com>
2026-03-08 20:40:24 +0900

    add:App.tsx にルート追加

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router'
 import { MainPage } from './pages/MainPage'
+import { LoginPage } from './pages/LoginPage'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
+      <Route path="/login" element={<LoginPage />} />
     </Routes>
   )
 }
