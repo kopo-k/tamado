af150b829afec4369c884cf5b5dd6caf7b523b9d
こぽー <tora1025oishi@gmail.com>
2026-03-09 18:08:02 +0900

    add:ルートを作成

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router'
 import { MainPage } from './pages/MainPage'
 import { LoginPage } from './pages/LoginPage'
+import { SignupPage } from './pages/SignupPage'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage/>} />
     </Routes>
   )
 }
